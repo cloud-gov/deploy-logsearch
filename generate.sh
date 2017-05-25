6008e8fbbf6eaa407359294178303ca632643ef2
Will Woodson <william.woodson@gsa.gov>
2017-05-25 07:26:47 -0600

    Working on migration to cloud config + consume tf outputs

--- generate.sh
+++ generate.sh
@@ -10,12 +10,16 @@ if [ ! -z "$1" ]; then
   SECRETS=$1
 fi
 if [ ! -z "$2" ]; then
-  MANIFEST=$2
+  ENVIRONENT=$2
+fi
+if [ ! -z "$3" ]; then
+  MANIFEST=$3
 fi
 
 spruce merge \
+  --prune terraform_outputs \
   $SCRIPTPATH/logsearch-deployment.yml \
   $SCRIPTPATH/logsearch-jobs.yml \
-  $SCRIPTPATH/logsearch-infrastructure-aws.yml \
+  $ENVIRONMENT \
   $SECRETS \
   > $MANIFEST
