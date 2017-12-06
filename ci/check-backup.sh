e7b8db2a210849f2fb6cc7757e732fe3f368913b
Joshua Carp <jm.carp@gmail.com>
2017-12-06 14:29:31 -0500

    Export recent platform log counts.

--- ci/check-backup.sh
+++ ci/check-backup.sh
@@ -6,7 +6,9 @@ file_size=0
 file_count=0
 now=$(date -u)
 
-log_count=$(curl -s "${ES_HOST}:${ES_PORT:-9200}/logs-app-*/_search?size=0" -d @<(cat <<EOF
+tempfile=$(mktemp)
+
+log_count=$(curl -s "${ES_HOST}:${ES_PORT:-9200}/${INDEX_PATTERN}/_search?size=0" -d @<(cat <<EOF
 {
   "query": {
     "range": {
@@ -20,17 +22,24 @@ log_count=$(curl -s "${ES_HOST}:${ES_PORT:-9200}/logs-app-*/_search?size=0" -d @
 EOF
 ) | jq -r '.hits.total')
 
-for idx in {10..19}; do
-  prefix=$(date -u +%Y/%m/%d/%H/%M --date "${now} -${idx} min")
-  for obj_size in $(aws s3 ls "s3://${BUCKET_NAME}/${prefix}/" | awk '{print $3}'); do
-    file_size=$((file_size + obj_size))
-    file_count=$((file_count + 1))
-  done
-done
-
-cat <<EOF | curl --data-binary @- "${GATEWAY_HOST}:${GATEWAY_PORT:-9091}/metrics/job/logsearch_backup/instance/${ENVIRONMENT}"
+cat <<EOF > ${tempfile}
 logsearch_backup_log_count {environment="${ENVIRONMENT}"} ${log_count}
+logsearch_backup_timestamp {environment="${ENVIRONMENT}"} $(date --date "${now}" +%s)
+EOF
+
+if [ -n "${BUCKET_NAME:-}" ]; then
+  for idx in {10..19}; do
+    prefix=$(date -u +%Y/%m/%d/%H/%M --date "${now} -${idx} min")
+    for obj_size in $(aws s3 ls "s3://${BUCKET_NAME}/${prefix}/" | awk '{print $3}'); do
+      file_size=$((file_size + obj_size))
+      file_count=$((file_count + 1))
+    done
+  done
+
+  cat <<EOF >> ${tempfile}
 logsearch_backup_file_size {environment="${ENVIRONMENT}"} ${file_size}
 logsearch_backup_file_count {environment="${ENVIRONMENT}"} ${file_count}
-logsearch_backup_timestamp {environment="${ENVIRONMENT}"} $(date +%s)
 EOF
+fi
+
+cat <<EOF | curl --data-binary @${tempfile} "${GATEWAY_HOST}:${GATEWAY_PORT:-9091}/metrics/job/logsearch_backup/instance/${ENVIRONMENT}"
