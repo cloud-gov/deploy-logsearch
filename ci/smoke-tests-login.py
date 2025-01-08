90b57112340b0e3ace0f26d2859dccc7bf200944
Jason A. Gambino <jason.gambino@gsa.gov>
2025-01-08 16:06:21 -0500

    chaning name

--- ci/smoke-tests-login.py
+++ ci/smoke-tests-login.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.DEBUG)
 if __name__ == "__main__":
     session = requests.Session()
 
-    logs = session.get('https://logs-deprecated.{}'.format(os.environ['CF_SYSTEM_DOMAIN']))
+    logs = session.get('https://logs-deprecated-waiting-for-delete.{}'.format(os.environ['CF_SYSTEM_DOMAIN']))
     print(logs.url, logs.status_code)
     assert logs.url == 'https://login.{}/login'.format(os.environ['CF_SYSTEM_DOMAIN'])
     assert logs.status_code == 200
@@ -23,5 +23,5 @@ if __name__ == "__main__":
         },
     )
     print(login.url, login.status_code)
-    assert login.url == 'https://logs-deprecated.{}/app/home'.format(os.environ['CF_SYSTEM_DOMAIN'])
+    assert login.url == 'https://logs-deprecated-waiting-for-delete.{}/app/home'.format(os.environ['CF_SYSTEM_DOMAIN'])
     assert login.status_code == 200
